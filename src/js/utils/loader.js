945d11c250c2e48e72308a826740ec7f2f1cd084
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-08 14:49:44 +0100

    ändrat loading

--- src/js/utils/loader.js
+++ src/js/utils/loader.js
@@ -1,7 +1,17 @@
-export function showLoader() {
-    document.querySelector("#loader").classList.remove("hidden")
+let loaderTimeout = null;
+
+export function showLoader(delay = 300) {
+    const loader = document.querySelector("#loader")
+
+    //Visa inte loader förrän 300ms efter laddningen påbörjat för att undvika att UI:n hoppar
+    loaderTimeout = setTimeout(() => {
+        loader.classList.remove("hidden")
+    }, delay)
 }
 
 export function hideLoader() {
-    document.querySelector("#loader").classList.add("hidden")
+    const loader = document.querySelector("#loader")
+    
+    clearTimeout(loaderTimeout)
+    loader.classList.add("hidden")
 }
